c373383cdaff1e9b9492ec673e174c3aa8e6eca2
Guilherme Almeida <gui.almeidacrz@gmail.com>
2020-01-29 22:14:00 -0300

    Criação dos campos para inclusao de noticias no form_inclusao_noticia e implementação de classes.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
 
 var connMysql = function(){
-    console.log('Conexao com o BD foi estabelecida')
     return connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -11,6 +10,5 @@ var connMysql = function(){
 }
 
 module.exports = function(){
-    console.log('O autoload carregou o modulo de conexao com o BD');
     return connMysql;
 }
